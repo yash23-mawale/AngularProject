869277330a59cb055f1e6018fa9c6d24fe86db5b
yash23-mawale <yashmawale190@gmail.com>
2025-08-12 11:32:33 +0530

    Angular Project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { AdminComponent } from './components/admin/admin.component';
+import { StudentComponent } from './components/student/student.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AdminComponent,StudentComponent,LoginComponent,SignupComponent,RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,36 @@
 import { Routes } from '@angular/router';
+import { AdminComponent } from './components/admin/admin.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { StudentComponent } from './components/student/student.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path:'',
+        redirectTo:'homepage',
+        pathMatch:'full'
+    },
+    {
+        path:'admin',
+        component:AdminComponent
+    },
+    {
+        path:'homepage',
+        component:HomepageComponent
+    },
+    {
+        path:'login',
+        component:LoginComponent
+    },
+    {
+        path:'signup',
+        component:SignupComponent
+    },
+    {
+        path:'student',
+        component:StudentComponent
+    }
+
+];
