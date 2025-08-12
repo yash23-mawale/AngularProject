f1a868c8a2f4470c0538890a116841b841ba891c
AniketDahire <aniketdahire476@gmail.com>
2025-08-12 14:55:00 +0530

    made changes in login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { login } from '../../model/interfaces';
+import { SignupComponent } from '../signup/signup.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule,SignupComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
